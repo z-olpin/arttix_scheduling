a8989a7acf732ec1dacc27ebe4e07470bdf2d720
Zachary Olpin <info@zacharyolp.in>
2019-09-11 04:21:39 -0600

    restructure table, buildings not separated by line

--- src/components/App.js
+++ src/components/App.js
@@ -4,86 +4,96 @@ const App = () => {
   return (
     <>
       <nav>
-        <div class="nav-wrapper blue-grey darken-1">
-          <a href="#" class="brand-logo" style={{ marginLeft: '1.5rem' }}>zchedul_</a>
-          <ul class="right">
+        <div className="nav-wrapper blue-grey darken-1 z-depth-3">
+          <a href="#" className="brand-logo" style={{ marginLeft: '1.5rem' }}>zchedul_</a>
+          <ul className="right">
+          <li><a href="#">Your Schedule</a></li>
             <li><a href="#">Full Schedule</a></li>
             <li><a href="#">Logout</a></li>
           </ul>
         </div>
       </nav>
-      <div className="container">
+      <div className="container" style={{width: '90vw'}}>
         <div className="row blue-grey-text" style={{ borderBottom: '0.2em solid #78909c' }}>
           <h3 style={{ fontWeight: 500 }}>Your week</h3>
         </div>
-        <table>
+        <table style={{tableLayout:'fixed'}}>
           <thead>
-            <tr className="blue-grey-text">
-              <th>BUILDING</th>
-              <th>Monday</th>
-              <th>Tuesday</th>
-              <th>Wednesday</th>
-              <th>Thursday</th>
-              <th>Friday</th>
-              <th>Saturday</th>
-              <th>Sunday</th>
+            <tr className="row blue-grey-text">
+              <th style={{width: '14.28%'}}>Monday</th>
+              <th style={{width: '14.28%'}}>Tuesday</th>
+              <th style={{width: '14.28%'}}>Wednesday</th>
+              <th style={{width: '14.28%'}}>Thursday</th>
+              <th style={{width: '14.28%'}}>Friday</th>
+              <th style={{width: '14.28%'}}>Saturday</th>
+              <th style={{width: '14.28%'}}>Sunday</th>
             </tr>
           </thead>
 
-          <tbody>
+          <tbody className="row">
             <tr>
-              <th>Capitol</th>
-              <td></td>
-              <td>9 &ndash; 2</td>
-              <td>2 &ndash; 8</td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
+            <td>
+              <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Delta Hall</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:00 &ndash; 2:00</span></div>
+              </div>
+            </td>
+            <td>
+            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Capitol</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:45 &ndash; 6:00</span></div>
+              </div>
+            </td>
+            <td>
+            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Rose Wagner</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>5:00 &ndash; 8:00</span></div>
+              </div>
+            </td>
+            <td>
+
+            </td>
+            <td>
+            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Delta Hall</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:45 &ndash; 2:00</span></div>
+              </div>
+            </td>
+            <td>
+            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Abravanel</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>1:45 &ndash; 6:00</span></div>
+              </div>
+            </td>
+            <td>
+
+            </td>
             </tr>
             <tr>
-              <th>Delta Hall</th>
-              <td>9 &ndash; 2</td>
               <td></td>
               <td></td>
               <td></td>
               <td></td>
+              <td>
+              <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Rose Wagner</div>
+                <hr style={{width: '80%'}}></hr>
+                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>2:30 &ndash; 6:00</span></div>
+              </div>
+              </td>
               <td></td>
               <td></td>
+
             </tr>
-            <tr>
-              <th>Abravanel</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>9 &ndash; 2</td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th>Rose Wagner</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th>Regent Street</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>9 &ndash; 2</td>
-            </tr>
+         
           </tbody>
         </table>
-        <button class="right waves-effect waves-light btn-small blue-grey lighten-1"
+        <button className="right waves-effect waves-light btn-small blue-grey lighten-1"
           style={{ marginTop: '3rem' }}>Add to Google Calendar</button>
       </div>
     </>
--- src/server/server.js
+++ src/server/server.js
@@ -16,7 +16,7 @@ server.get('/', (req, res) => {
         <title>ArtTix Scheduloor</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
       </head>
-      <body>
+      <body style="font-family: helvetica">
         <div id="app">${initialMarkup}</div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
             
