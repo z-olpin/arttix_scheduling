01deb7e1d7d49a96b8caf46f89fbc0f2dfce930a
Zachary Olpin <info@zacharyolp.in>
2019-09-12 00:25:54 -0600

    dynamically create schedule table from dummy data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,44 @@
 import React from 'react';
 
 const App = () => {
+
+  // Dummy Data
+  const empSched = {
+    shifts: [
+      {
+        building: 'Capitol',
+        calDay: '2019-09-08',
+        weekDay: 'monday',
+        startTime: '13:45',
+        endTime: '18:00'
+      },
+      {
+        building: 'Regent Street',
+        calDay: '2019-10-08',
+        weekDay: 'tuesday',
+        startTime: '09:45',
+        endTime: '14:00'
+      },
+      {
+        building: 'Abravanel',
+        calDay: '2019-13-08',
+        weekDay: 'thursday',
+        startTime: '14:00',
+        endTime: '18:00'
+      },
+      {
+        building: 'Delta Hall',
+        calDay: '2019-16-08',
+        weekDay: 'sunday',
+        startTime: '18:00',
+        endTime: '21:00'
+      }
+    ]
+  }
+
+  const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+
+  // Doesn't work for multiple shifts in one day because find() / indexOf() return first match.
   return (
     <>
       <nav>
@@ -13,6 +51,7 @@ const App = () => {
           </ul>
         </div>
       </nav>
+
       <div className="container" style={{width: '90vw'}}>
         <div className="row blue-grey-text" style={{ borderBottom: '0.2em solid #78909c' }}>
           <h3 style={{ fontWeight: 500 }}>Your week</h3>
@@ -29,68 +68,30 @@ const App = () => {
               <th style={{width: '14.28%'}}>Sunday</th>
             </tr>
           </thead>
-
           <tbody className="row">
             <tr>
-            <td>
-              <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Delta Hall</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:00 &ndash; 2:00</span></div>
-              </div>
-            </td>
-            <td>
-            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Capitol</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:45 &ndash; 6:00</span></div>
-              </div>
-            </td>
-            <td>
-            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Rose Wagner</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>5:00 &ndash; 8:00</span></div>
-              </div>
-            </td>
-            <td>
-
-            </td>
-            <td>
-            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Delta Hall</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>9:45 &ndash; 2:00</span></div>
-              </div>
-            </td>
-            <td>
-            <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Abravanel</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>1:45 &ndash; 6:00</span></div>
-              </div>
-            </td>
-            <td>
-
-            </td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-              <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
-                <div className="card-title center-align" style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>Rose Wagner</div>
-                <hr style={{width: '80%'}}></hr>
-                <div className="card-title center-align" style={{fontSize: '1.2rem'}}><span>2:30 &ndash; 6:00</span></div>
-              </div>
-              </td>
-              <td></td>
-              <td></td>
-
-            </tr>
-         
+              {weekdays.map(day => {
+                const shiftInd = empSched.shifts.map(s=>s.weekDay).indexOf(day)
+                if (shiftInd >= 0) {
+                  return (
+                    <td>
+                      <div className="card blue-grey lighten-3 z-depth-2" style={{padding: '0.5rem'}}>
+                        <div className="card-title center-align"
+                          style={{color: '#892d2d', fontSize: '1.2rem', fontWeight: '500', textTransform:'uppercase'}}>
+                            {empSched.shifts[shiftInd].building}
+                        </div>
+                        <hr style={{width: '80%'}}></hr>
+                        <div className="card-title center-align" style={{fontSize: '1.2rem'}}>
+                          <span>{empSched.shifts[shiftInd].startTime + ' - ' +  empSched.shifts[shiftInd].endTime}</span>
+                        </div>
+                      </div>
+                    </td>
+                  )
+                } else {
+                  return <td></td>
+                }
+              })}
+            </tr>         
           </tbody>
         </table>
         <button className="right waves-effect waves-light btn-small blue-grey lighten-1"
