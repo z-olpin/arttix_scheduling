71c0ed932751b2b137d84acb346cc8b17769aef8
Zachary Olpin <info@zacharyolp.in>
2019-09-17 03:46:58 -0600

    hard code full real schedule and add user input suggestions

--- src/components/App.js
+++ src/components/App.js
@@ -3,66 +3,124 @@ import empSched from "../empSched";
 
 const App = () => {
   const [user, setUser] = useState('')
-  const [userShifts, setUserShifts] = useState([])
-
-  useEffect(() => setUserShifts([...empSched.shifts.filter(s => s.employee == user)]), [user])
-
+  const [AMShifts, setAMShifts] = useState([])
+  const [afternoonShifts, setAfternoonShifts] = useState([])
+  const [showShifts, setShowShifts] = useState([])
+  const [suggestions, setSuggestions] = useState(['abigail', 'alicia', 'jacque', 'jacqueline', 'jaiden', 'jaron', 'jonathan', 'josie', 'kat', 'lisa', 'ocia', 'noorah', 'payton', 'sam', 'sammee', 'tim', 'whit', 'zach'])
   const weekdays = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 
-  // Doesn't work for multiple shifts in one day because find() / indexOf() return first match.
+  useEffect(() => setAMShifts([...empSched.shifts.filter(s => s.employee == user && s.startTime.includes('am'))]), [user])
+
+  useEffect(() => setAfternoonShifts([...empSched.shifts.filter(s => s.employee == user && s.startTime.includes('pm') && s.startTime < '4')]), [user])
+
+  useEffect(() => setShowShifts([...empSched.shifts.filter(s => s.employee == user && s.startTime > '4' && s.startTime.includes('pm'))]), [user])
+
+  const handleUserChange = e => {
+    e.preventDefault()
+    setUser(e.target.value)
+  }
+
   return (
     <>
       <nav style={{ marginBottom: '1rem' }}>
-        <div className="nav-wrapper z-depth-3" style={{backgroundColor: "#0000ff"}}>
+        <div className="nav-wrapper z-depth-3" style={{ backgroundColor: "#0000ff" }}>
           <a href="#" className="brand-logo" style={{ marginLeft: '1.5rem' }}>zchedul_</a>
           <ul className="right valign-wrapper" style={{ marginRight: '1.2rem' }}>
             <li>
               <div style={{ marginRight: '1.2rem' }}>USER:</div>
             </li>
             <li>
-              <select onChange={e => setUser(e.target.value)} className="browser-default" style={{ color: '#ffffff', backgroundColor: 'transparent' }}>
-                <option value="" selected></option>
-                <option value="zach">Zach</option>
-                <option value="ana">Ana</option>
-                <option value="bob">Bob</option>
-              </select>
+              <input type="text" onChange={handleUserChange} value={user} list="suggestions" style={{ color: '#ffffff' }}>
+              </input>
+              <datalist id="suggestions">
+                {suggestions.map(s => <option value={s}></option>)}
+              </datalist>
             </li>
           </ul>
         </div>
       </nav>
-
-
       <div className="container" style={{ width: '90vw' }}>
-        <div className="row" style={{ color: '#0000ff', borderBottom: '0.2em solid #0000ff' }}>
-          <h3 style={{ fontWeight: 500 }}>Your week</h3>
+        <div className="row" style={{ borderBottom: '0.2em solid #0000ff' }}>
+          <h3 style={{ fontWeight: 500, color: '#0000ff' }}>Your week</h3>
         </div>
+        <h6 style={{ fontSize: '1.4rem', fontStyle: 'italic', color: 'grey' }}>09/16 &mdash;  09/22</h6>
         <table style={{ tableLayout: 'fixed' }}>
           <thead>
             <tr className="row">
-              <th style={{ width: '14.28%' }}>Monday</th>
-              <th style={{ width: '14.28%' }}>Tuesday</th>
-              <th style={{ width: '14.28%' }}>Wednesday</th>
-              <th style={{ width: '14.28%' }}>Thursday</th>
-              <th style={{ width: '14.28%' }}>Friday</th>
-              <th style={{ width: '14.28%' }}>Saturday</th>
-              <th style={{ width: '14.28%' }}>Sunday</th>
+              <th className="center-align" style={{ width: '14.28%' }}></th>
+              <th className="center-align" style={{ width: '14.28%' }}>Monday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Tuesday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Wednesday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Thursday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Friday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Saturday</th>
+              <th className="center-align" style={{ width: '14.28%' }}>Sunday</th>
             </tr>
           </thead>
           <tbody className="row">
             <tr>
+              <th>AM</th>
               {weekdays.map(day => {
-                const shiftInd = userShifts.map(s => s.weekDay).indexOf(day)
+                const shiftInd = AMShifts.map(s => s.weekDay).indexOf(day)
                 if (shiftInd >= 0) {
                   return (
                     <td>
                       <div className="card z-depth-2" style={{ border: '2px solid #0000ff', padding: '0.5rem' }}>
                         <div className="card-title center-align"
-                          style={{fontSize: '1.2rem', fontWeight: '700', textTransform: 'uppercase' }}>
-                          {userShifts[shiftInd].building}
+                          style={{ fontSize: '1.2rem', fontWeight: '700', textTransform: 'uppercase' }}>
+                          {AMShifts[shiftInd].building}
                         </div>
                         <hr style={{ width: '80%' }}></hr>
                         <div className="card-title center-align" style={{ fontSize: '1.2rem' }}>
-                          <span>{userShifts[shiftInd].startTime + ' - ' + userShifts[shiftInd].endTime}</span>
+                          <span>{AMShifts[shiftInd].startTime + ' - ' + AMShifts[shiftInd].endTime}</span>
+                        </div>
+                      </div>
+                    </td>
+                  )
+                } else {
+                  return <td></td>
+                }
+              })}
+            </tr>
+            <tr>
+              <th>PM</th>
+              {weekdays.map(day => {
+                const shiftInd = afternoonShifts.map(s => s.weekDay).indexOf(day)
+                if (shiftInd >= 0) {
+                  return (
+                    <td>
+                      <div className="card z-depth-2" style={{ border: '2px solid #0000ff', padding: '0.5rem' }}>
+                        <div className="card-title center-align"
+                          style={{ fontSize: '1.2rem', fontWeight: '700', textTransform: 'uppercase' }}>
+                          {afternoonShifts[shiftInd].building}
+                        </div>
+                        <hr style={{ width: '80%' }}></hr>
+                        <div className="card-title center-align" style={{ fontSize: '1.2rem' }}>
+                          <span>{afternoonShifts[shiftInd].startTime + ' - ' + afternoonShifts[shiftInd].endTime}</span>
+                        </div>
+                      </div>
+                    </td>
+                  )
+                } else {
+                  return <td></td>
+                }
+              })}
+            </tr>
+            <tr>
+              <th>Show</th>
+              {weekdays.map(day => {
+                const shiftInd = showShifts.map(s => s.weekDay).indexOf(day)
+                if (shiftInd >= 0) {
+                  return (
+                    <td>
+                      <div className="card z-depth-2" style={{ border: '2px solid #0000ff', padding: '0.5rem' }}>
+                        <div className="card-title center-align"
+                          style={{ fontSize: '1.2rem', fontWeight: '700', textTransform: 'uppercase' }}>
+                          {showShifts[shiftInd].building}
+                        </div>
+                        <hr style={{ width: '80%' }}></hr>
+                        <div className="card-title center-align" style={{ fontSize: '1.2rem' }}>
+                          <span>{showShifts[shiftInd].startTime + ' - ' + showShifts[shiftInd].endTime}</span>
                         </div>
                       </div>
                     </td>
@@ -75,7 +133,7 @@ const App = () => {
           </tbody>
         </table>
         <button className="right waves-effect waves-light btn-small"
-          style={{ marginTop: '3rem', backgroundColor: '#0000ff'}}>Add to Google Calendar</button>
+          style={{ marginTop: '3rem', backgroundColor: '#0000ff' }}>Add to Google Calendar</button>
       </div>
     </>
   )
--- src/empSched.js
+++ src/empSched.js
@@ -1,93 +1,772 @@
 const empSched = {
   shifts: [
     {
-      employee: 'zach',
-      building: 'Capitol',
-      calDay: '2019-09-08',
+      employee: 'ocia',
+      building: 'abravanel',
+      calDay: '09-16-2019',
       weekDay: 'monday',
-      startTime: '13:45',
-      endTime: '18:00'
+      startTime: '9:45am',
+      endTime: '2:00pm'
     },
     {
-      employee: 'zach',
-      building: 'Regent Street',
-      calDay: '2019-10-08',
+      employee: 'sammee',
+      building: 'abravanel',
+      calDay: '09-17-2019',
       weekDay: 'tuesday',
-      startTime: '09:45',
-      endTime: '14:00'
+      startTime: '9:45am',
+      endTime: '2:00pm'
     },
     {
-      employee: 'zach',
-      building: 'Abravanel',
-      calDay: '2019-13-08',
-      weekDay: 'thursday',
-      startTime: '14:00',
-      endTime: '18:00'
-    },
-    {
-      employee: 'zach',
-      building: 'Delta Hall',
-      calDay: '2019-16-08',
-      weekDay: 'sunday',
-      startTime: '18:00',
-      endTime: '21:00'
-    },
-
-    {
-      employee: 'ana',
-      building: 'Abravanel',
-      calDay: '2019-09-08',
-      weekDay: 'tuesday',
-      startTime: '13:45',
-      endTime: '18:00'
-    },
-    {
-      employee: 'ana',
-      building: 'Regent Street',
-      calDay: '2019-10-08',
+      employee: 'sam',
+      building: 'abravanel',
+      calDay: '09-18-2019',
       weekDay: 'wednesday',
-      startTime: '09:45',
-      endTime: '14:00'
+      startTime: '9:45am',
+      endTime: '2:00pm'
     },
     {
-      employee: 'ana',
-      building: 'Rose Wagner',
-      calDay: '2019-13-08',
+      employee: 'jacqueline',
+      building: 'abravanel',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'ocia',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'abravanel',
+      calDay: '09-21-2019',
       weekDay: 'saturday',
-      startTime: '14:00',
-      endTime: '18:00'
+      startTime: '9:45am',
+      endTime: '2:00pm'
     },
     {
-      employee: 'ana',
-      building: 'Delta Hall',
-      calDay: '2019-16-08',
-      weekDay: 'friday',
-      startTime: '18:00',
-      endTime: '21:00'
-    },
-    {
-      employee: 'bob',
-      building: 'Abravanel',
-      calDay: '2019-13-08',
-      weekDay: 'friday',
-      startTime: '14:00',
-      endTime: '18:00'
-    },
-    {
-      employee: 'bob',
-      building: 'Regent Street',
-      calDay: '2019-16-08',
-      weekDay: 'sunday',
-      startTime: '18:00',
-      endTime: '21:00'
-    },
-    {
-      employee: 'bob',
-      building: 'Rose Wagner',
-      calDay: '2019-16-08',
+      employee: 'whit',
+      building: 'abravanel',
+      calDay: '09-16-2019',
       weekDay: 'monday',
-      startTime: '18:00',
-      endTime: '21:00'
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'abravanel',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'abravanel',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'abravanel',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jaiden',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'ocia',
+      building: 'abravanel',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'abravanel',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'ocia',
+      building: 'abravanel',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'abravanel',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jaiden',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jonathan',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jacque',
+      building: 'abravanel',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'abravanel',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'lisa',
+      building: 'delta hall',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'sam',
+      building: 'delta hall',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'delta hall',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jaiden',
+      building: 'delta hall',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'sam',
+      building: 'delta hall',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jacque',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'sammee',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'josie',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'lisa',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jaiden',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'delta hall',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'delta hall',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'josie',
+      building: 'delta hall',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'delta hall',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'alicia',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'tim',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '2:15pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '6:15pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'payton',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '6:15pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '6:15pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'jonathan',
+      building: 'delta hall',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '6:15pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'zach',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '6:45pm',
+      endTime: '9:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '6:45pm',
+      endTime: '9:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '6:45pm',
+      endTime: '9:00pm'
+    },
+    {
+      employee: 'josie',
+      building: 'delta hall',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '6:45pm',
+      endTime: '9:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '6:45pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '6:45pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'jacqueline',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '6:45pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'sam',
+      building: 'delta hall',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '6:45pm',
+      endTime: '8:30pm'
+    },
+    {
+      employee: 'alicia',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'tim',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'ocia',
+      building: 'delta hall',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jonathan',
+      building: 'regent street',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'whit',
+      building: 'regent street',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'regent street',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jaiden',
+      building: 'regent street',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '12:15pm',
+      endTime: '2:30pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'rose wagner',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'rose wagner',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'josie',
+      building: 'rose wagner',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'lisa',
+      building: 'rose wagner',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'rose wagner',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jacque',
+      building: 'rose wagner',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '9:45am',
+      endTime: '2:00pm'
+    },
+    {
+      employee: 'jacque',
+      building: 'rose wagner',
+      calDay: '09-16-2019',
+      weekDay: 'monday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'kat',
+      building: 'rose wagner',
+      calDay: '09-17-2019',
+      weekDay: 'tuesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'zach',
+      building: 'rose wagner',
+      calDay: '09-18-2019',
+      weekDay: 'wednesday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'rose wagner',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'rose wagner',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '2:15pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'rose wagner',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'rose wagner',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'rose wagner',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jonathan',
+      building: 'rose wagner',
+      calDay: '09-22-2019',
+      weekDay: 'saturday',
+      startTime: '1:45pm',
+      endTime: '6:00pm'
+    },
+    {
+      employee: 'jaron',
+      building: 'rose wagner',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'abigail',
+      building: 'rose wagner',
+      calDay: '09-19-2019',
+      weekDay: 'thursday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'noorah',
+      building: 'rose wagner',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'payton',
+      building: 'rose wagner',
+      calDay: '09-20-2019',
+      weekDay: 'friday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'ivana',
+      building: 'rose wagner',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
+    },
+    {
+      employee: 'jonathan',
+      building: 'rose wagner',
+      calDay: '09-21-2019',
+      weekDay: 'saturday',
+      startTime: '5:45pm',
+      endTime: '8:00pm'
     }
   ]
 }
