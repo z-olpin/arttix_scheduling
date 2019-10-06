1f64ee4306a0b32b3fa68368ecdbebacb9077587
Zachary Olpin <info@zacharyolp.in>
2019-10-06 17:18:34 -0600

    webpack build

--- src/server/server.js
+++ src/server/server.js
@@ -1,29 +1,31 @@
 import express from 'express';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import App from '../components/App';
+// import App from '../components/App';
+
+const PORT = process.env.PORT || 5000
 
 const server = express();
+
 
 server.use(express.static('dist'));
 
 server.get('/', (req, res) => {
-  const initialMarkup = ReactDOMServer.renderToString(<App />);
+  // const initialMarkup = ReactDOMServer.renderToString(<App />);
 
-  res.send(`
-    <html>
-      <head>
-        <title>zschedul_</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-      </head>
-      <body style="font-family: helvetica">
-        <div id="app">${initialMarkup}</div>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+  // res.send(`
+  //   <html>
+  //     <head>
+  //       <title>zschedul_</title>
+  //       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  //     </head>
+  //     <body style="font-family: helvetica">
+  //       <div id="app">${initialMarkup}</div>
+  //       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
-        <script src="/main.js"></script>
-      </body>
-    </html>
-  `)
+  //       <script src="/main.js"></script>
+  //     </body>
+  //   </html>
+  // `)
+res.send('<h1>Hello</h1>')
 });
 
-server.listen(4242, () => console.log('Server is running...'));
+server.listen(PORT, () => console.log('Server is running...'));
