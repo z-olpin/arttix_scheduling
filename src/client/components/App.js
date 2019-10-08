de283cf09ec8644ecfceeea1d7bfefdcb4af73fc
Zachary Olpin <info@zacharyolp.in>
2019-10-08 01:04:11 -0600

    fetch and display shift data on grid

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -1,32 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import '../../public/index.css';
 
 const App = () => {
   const [user, setUser] = useState('')
-  const [suggestions, setSuggestions] = useState(['zach', 'alice', 'bob', 'charlie'])
-  const [shifts, setShifts] = useState([])
+  const [suggestions, setSuggestions] = useState(['Zach', 'Alice', 'Bob', 'Charlie', 'Doug'])
+  const [shifts, setShifts] = useState()
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/employees/${user}/shifts`, {method: 'GET'})
+    .then(r => r.json())
+    .then(r => setShifts(r))
+  }, [user])
   
   const handleUserChange = e => {
     e.preventDefault()
-    setUser(e.target.value)
-  }
-
-  const getShifts = e => {
-    e.preventDefault()
-    fetch(`http://localhost:5000/employees/${user}/shifts`, {method: 'GET'})
-    .then(r=>r.json()).then(j => setShifts(j))
+    setUser(e.target.value.slice(0,1).toLowerCase() + e.target.value.slice(1))
   }
 
   return (
     <>
-      <nav style={{ marginBottom: '1rem' }}>
-        <div className="nav-wrapper z-depth-3" style={{ backgroundColor: "#0000ff" }}>
+      <nav style={{ marginBottom: '1rem', backgroundColor: 'rgb(0,0,255)' }}>
+        <div >
           <a href="#" className="brand-logo" style={{ marginLeft: '1.5rem' }}>zchedul_</a>
           <ul className="right valign-wrapper" style={{ marginRight: '1.2rem' }}>
             <li>
               <div style={{ marginRight: '1.2rem' }}>USER:</div>
             </li>
             <li>
-              <input type="text" onChange={handleUserChange} value={user} list="suggestions" style={{ color: '#ffffff' }}>
+              <input id="user-input" type="select" onChange={handleUserChange} list="suggestions">
               </input>
               <datalist id="suggestions">
                 {suggestions.map(s => <option value={s}></option>)}
@@ -35,9 +36,33 @@ const App = () => {
           </ul>
         </div>
       </nav>
-      <div>{(shifts.length >= 1) ? shifts.map(shift => <ul><li>{shift.name}</li><li>{shift.start}</li><li>{shift.end}</li></ul>) : <></>}</div>
-      <button onClick={getShifts}>Get Shifts</button>
-      
+      <div id="week-slider">
+        <button id='week-left'>&#8592;</button>
+        <h3 id='week-title'>This week</h3>
+        <button id='week-right'>&#8594;</button>
+      </div>
+      <div id="schedule">
+        {
+          ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+            .map(day => <div class="column-title" id={day.toLowerCase()}>{day}</div>)
+        }
+        {
+          ['8a', '10a', '12p', '2p', '4p', '6p', '8p']
+            .map(time =>
+              <div id={'time' + time}>
+                {time.match(/\d{1,2}/)[0] + ' ' + time[time.length-1].toUpperCase() + 'M'}
+              </div>
+            )
+        }
+        {
+          shifts && shifts.map(shift => <div class='shift' id={'shift' + shift.shift_id}>
+              {shift.building.split(' ').map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ')}
+              <p></p>
+              {shift.start.match(/\d\d\:\d\d/)[0]} - {shift.end.match(/\d\d\:\d\d/)[0]}
+            </div>
+          )
+        }
+      </div>      
     </>
   )
 }
