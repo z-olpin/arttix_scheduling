637d31484ae49e9f306de1b4288cea2b66280bac
Zachary Olpin <info@zacharyolp.in>
2019-10-14 05:00:48 -0600

    shifts displayed on grid w respect to start and end times

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from 'react';
 import '../../public/index.css';
 
 const App = () => {
-  const [user, setUser] = useState('')
+  const [user, setUser] = useState()
   const [suggestions, setSuggestions] = useState(['Zach', 'Alice', 'Bob', 'Charlie', 'Doug'])
-  const [shifts, setShifts] = useState()
+  const [shifts, setShifts] = useState([])
 
   useEffect(() => {
     fetch(`http://localhost:5000/employees/${user}/shifts`, {method: 'GET'})
@@ -20,7 +20,7 @@ const App = () => {
   return (
     <>
       <nav style={{ marginBottom: '1rem', backgroundColor: 'rgb(0,0,255)' }}>
-        <div >
+        <div>
           <a href="#" className="brand-logo" style={{ marginLeft: '1.5rem' }}>zchedul_</a>
           <ul className="right valign-wrapper" style={{ marginRight: '1.2rem' }}>
             <li>
@@ -37,30 +37,19 @@ const App = () => {
         </div>
       </nav>
       <div id="week-slider">
-        <button id='week-left'>&#8592;</button>
+        <button className="week-button">&#8592;</button>
         <h3 id='week-title'>This week</h3>
-        <button id='week-right'>&#8594;</button>
+        <button className="week-button">&#8594;</button>
       </div>
       <div id="schedule">
-        {
-          ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-            .map(day => <div class="column-title" id={day.toLowerCase()}>{day}</div>)
-        }
-        {
-          ['8a', '10a', '12p', '2p', '4p', '6p', '8p']
-            .map(time =>
-              <div id={'time' + time}>
-                {time.match(/\d{1,2}/)[0] + ' ' + time[time.length-1].toUpperCase() + 'M'}
-              </div>
-            )
-        }
-        {
-          shifts && shifts.map(shift => <div class='shift' id={'shift' + shift.shift_id}>
-              {shift.building.split(' ').map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ')}
-              <p></p>
-              {shift.start.match(/\d\d\:\d\d/)[0]} - {shift.end.match(/\d\d\:\d\d/)[0]}
-            </div>
-          )
+        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => <div class="column-title" id={day.toLowerCase()}>{day}</div>)}
+        {['8a', '10a', '12p', '2p', '4p', '6p', '8p'].map(time => <div id={'time' + time}>{time.match(/\d{1,2}/)[0] + ' ' + time[time.length-1].toUpperCase() + 'M'}</div>)}
+        {shifts &&
+          shifts.map(shift => <div className={'shift ' + shift.dow} style={{gridRow: shift.gridSpan.begin + ' / ' + shift.gridSpan.span}}>
+            {shift.building.split(' ').map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ')}
+            <p></p>
+            {`${(shift.start_hour > 12) ? shift.start_hour - 12 : shift.start_hour}:${shift.start_min}`} - {`${(shift.end_hour > 12) ? shift.end_hour - 12 : shift.end_hour}:${shift.end_min}`}
+          </div>)
         }
       </div>      
     </>
--- src/server/server.js
+++ src/server/server.js
@@ -20,11 +20,53 @@ server.get('/', (req, res) => {
 })
 
 server.get('/employees/:name/shifts', async (req, res) => {
-  const result = await pool.query("select shifts.shift_id, shifts.start, shifts.end, shifts.building_id, buildings.name as building, employees.name from shifts, employees, buildings where employees.name=$1 and shifts.employee_id=employees.employee_id and shifts.building_id=buildings.building_id", [req.params.name])
+  const dows = {
+    '1': 'monday',
+    '2': 'tuesday',
+    '3': 'wednesday',
+    '4': 'thursday',
+    '5': 'friday',
+    '6': 'saturday',
+    '7': 'sunday'
+  }
+
+  const hourMap =  {
+    '8': 2,
+    '9': 6,
+    '10': 10,
+    '11': 14,
+    '12': 18,
+    '13': 22,
+    '14': 26,
+    '15': 30,
+    '16': 34,
+    '17': 38,
+    '18': 42,
+    '19': 46,
+    '20': 50,
+    '21': 54,
+    '22': 58
+  }
+
+  const minMap = {
+    '00': 0,
+    '15': 1,
+    '30': 2,
+    '45': 3
+  }
+
+  const result = await pool.query('select extract(isodow from start) as dow, extract(hour from start) as start_hour, extract(minute from start) as start_min, extract(hour from "end") as end_hour, extract(minute from "end") as end_min, shifts.shift_id, shifts.start, shifts.end, shifts.building_id, buildings.name as building, employees.name from shifts, employees, buildings where employees.name=$1 and shifts.employee_id=employees.employee_id and shifts.building_id=buildings.building_id order by shifts.start asc', [req.params.name])
   let r = []
   for (let row of result.rows) {
     row.start = row.start.toLocaleString('en-us', {hour12: false})
     row.end = row.end.toLocaleString('en-us', {hour12: false})
+    row.dow = dows[row.dow]
+    if (row.end_min === 0) row.end_min = '00'
+    if (row.start_min === 0) row.start_min = '00'
+    row.gridSpan = {
+      begin: hourMap[row.start_hour] + minMap[row.start_min], 
+      span: hourMap[row.end_hour] + minMap[row.end_min]
+    }
     r.push(row)
   }
   res.json(r)
