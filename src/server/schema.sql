08a09cc08486b6d6fd5cc5b9c234246d2d5203a1
Zachary Olpin <info@zacharyolp.in>
2019-10-14 04:58:03 -0600

    add extra shifts

--- src/server/schema.sql
+++ src/server/schema.sql
@@ -5,7 +5,14 @@ CREATE TABLE shifts (shift_id BIGSERIAL PRIMARY KEY, building_id INT REFERENCES 
 INSERT INTO employees (name) VALUES ('zach'), ('alice'), ('bob'), ('charlie'), ('dave');
 INSERT INTO buildings (name) VALUES ('abravanel hall'), ('capitol'), ('delta hall'), ('regent street'), ('rose wagner');
 INSERT INTO shifts (building_id, start, "end", employee_id) VALUES 
-  ((SELECT building_id from buildings where name='abravanel hall'), '2019-10-07 09:45', '2019-10-07 14:00', (select employee_id from employees where name='zach')),
-  ((SELECT building_id from buildings where name='delta hall'), '2019-10-08 09:45', '2019-10-08 14:00', (select employee_id from employees where name='charlie')),
-  ((SELECT building_id from buildings where name='capitol'), '2019-10-09 13:45', '2019-10-09 18:00', (select employee_id from employees where name='bob')),
-  ((SELECT building_id from buildings where name='abravanel hall'), '2019-10-07 09:45', '2019-10-07 14:00', (select employee_id from employees where name='dave'));
+  ((SELECT building_id from buildings where name='abravanel hall'), '2019-10-14 09:45', '2019-10-14 14:00', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='delta hall'), '2019-10-14 09:45', '2019-10-14 14:00', (select employee_id from employees where name='charlie')),
+  ((SELECT building_id from buildings where name='capitol'), '2019-10-14 13:45', '2019-10-14 18:00', (select employee_id from employees where name='bob')),
+  ((SELECT building_id from buildings where name='abravanel hall'), '2019-10-14 09:45', '2019-10-14 14:00', (select employee_id from employees where name='dave')),
+  ((SELECT building_id from buildings where name='regent street'), '2019-10-16 09:45', '2019-10-16 14:00', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='regent street'), '2019-10-16 13:45', '2019-10-16 18:00', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='rose wagner'), '2019-10-17 13:45', '2019-10-17 18:00', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='rose wagner'), '2019-10-17 17:45', '2019-10-17 20:00', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='capitol'), '2019-10-19 17:45', '2019-10-19 20:30', (select employee_id from employees where name='zach')),
+  ((SELECT building_id from buildings where name='delta hall'), '2019-10-20 09:45', '2019-10-20 14:00', (select employee_id from employees where name='zach'));
+
