a669c1cba8afb7e84f83e4b82ee65e5da10e880d
Zachary Olpin <info@zacharyolp.in>
2019-10-16 01:06:24 -0600

    group shift objects by day to render single shift div

--- src/client/components/App.js
+++ src/client/components/App.js
@@ -5,16 +5,25 @@ const App = () => {
   const [user, setUser] = useState()
   const [suggestions, setSuggestions] = useState(['Zach', 'Alice', 'Bob', 'Charlie', 'Doug'])
   const [shifts, setShifts] = useState([])
+  const WEEKDAY_COLUMNS = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+  const TIME_ROWS = ['8a', '10a', '12p', '2p', '4p', '6p', '8p']
 
   useEffect(() => {
     fetch(`http://localhost:5000/employees/${user}/shifts`, {method: 'GET'})
     .then(r => r.json())
+    .then(rows => groupShiftsByDay(rows))
     .then(r => setShifts(r))
   }, [user])
   
   const handleUserChange = e => {
     e.preventDefault()
     setUser(e.target.value.slice(0,1).toLowerCase() + e.target.value.slice(1))
+  }
+
+  const groupShiftsByDay = (rows) => {
+    let _shifts = []
+    Array.from(new Set(rows.map(row => row.doy))).map(doy => _shifts.push(rows.filter(row => row.doy == doy)))
+    return _shifts
   }
 
   return (
@@ -42,14 +51,24 @@ const App = () => {
         <button className="week-button">&#8594;</button>
       </div>
       <div id="schedule">
-        {['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'].map(day => <div class="column-title" id={day.toLowerCase()}>{day}</div>)}
-        {['8a', '10a', '12p', '2p', '4p', '6p', '8p'].map(time => <div id={'time' + time}>{time.match(/\d{1,2}/)[0] + ' ' + time[time.length-1].toUpperCase() + 'M'}</div>)}
-        {shifts &&
-          shifts.map(shift => <div className={'shift ' + shift.dow} style={{gridRow: shift.gridSpan.begin + ' / ' + shift.gridSpan.span}}>
-            {shift.building.split(' ').map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ')}
-            <p></p>
-            {`${(shift.start_hour > 12) ? shift.start_hour - 12 : shift.start_hour}:${shift.start_min}`} - {`${(shift.end_hour > 12) ? shift.end_hour - 12 : shift.end_hour}:${shift.end_min}`}
-          </div>)
+        {WEEKDAY_COLUMNS
+          .map(day => <div className="column-title" id={day.toLowerCase()}>{day}</div>)}
+        {TIME_ROWS
+          .map(time => <div id={'time' + time}>{time.match(/\d{1,2}/)[0] + ' ' + time[time.length-1].toUpperCase() + 'M'}</div>)}
+        {shifts && shifts
+          .map(day => 
+            <div className={'shift ' + day[0].dow} style={{gridRow: day[0].gridStart + ' / ' + day[day.length-1].gridEnd}}>
+                {day[0].building.split(' ').map(w => w.slice(0,1).toUpperCase() + w.slice(1)).join(' ')}
+              <p></p>
+                {`
+                  ${(day[0].start_hour > 12)
+                    ? day[0].start_hour - 12
+                    : day[0].start_hour}:${day[0].start_min}`} - {`${(day[day.length-1].end_hour > 12)
+                      ? day[day.length-1].end_hour - 12
+                      : day[day.length-1].end_hour}:${day[day.length-1].end_min}
+                `}
+            </div>
+          )
         }
       </div>      
     </>
