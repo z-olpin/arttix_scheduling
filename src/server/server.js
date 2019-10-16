837a7d5a13a1bcc7137aca5ddac85da29b0d3bab
Zachary Olpin <info@zacharyolp.in>
2019-10-16 01:16:57 -0600

    convert format of query return

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const { Pool } = require('pg')
+const express = require('express')
+const { Pool} = require('pg')
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
@@ -28,7 +28,7 @@ server.get('/employees/:name/shifts', async (req, res) => {
     '5': 'friday',
     '6': 'saturday',
     '7': 'sunday'
-  }
+}
 
   const hourMap =  {
     '8': 2,
@@ -46,30 +46,36 @@ server.get('/employees/:name/shifts', async (req, res) => {
     '20': 50,
     '21': 54,
     '22': 58
-  }
+}
 
   const minMap = {
     '00': 0,
     '15': 1,
     '30': 2,
     '45': 3
-  }
+}
 
-  const result = await pool.query('select extract(isodow from start) as dow, extract(hour from start) as start_hour, extract(minute from start) as start_min, extract(hour from "end") as end_hour, extract(minute from "end") as end_min, shifts.shift_id, shifts.start, shifts.end, shifts.building_id, buildings.name as building, employees.name from shifts, employees, buildings where employees.name=$1 and shifts.employee_id=employees.employee_id and shifts.building_id=buildings.building_id order by shifts.start asc', [req.params.name])
-  let r = []
-  for (let row of result.rows) {
+  let result = await pool.query(
+    'select extract(isodow from start) as dow, extract(doy from start) as doy, extract(hour from start) as start_hour, extract(minute from start) as start_min, extract(hour from "end") as end_hour, extract(minute from "end") as end_min, shifts.shift_id::integer, shifts.start, shifts.end, shifts.building_id, buildings.name as building, employees.name from shifts, employees, buildings where employees.name=$1 and shifts.employee_id=employees.employee_id and shifts.building_id=buildings.building_id order by shifts.start asc', [req.params.name]
+  )
+  // Convert to string in query (::text) or after?
+
+  // Do all this on server or client?
+
+  result = result.rows.map(row => {
     row.start = row.start.toLocaleString('en-us', {hour12: false})
     row.end = row.end.toLocaleString('en-us', {hour12: false})
+    row.start_min = row.start_min.toString().padStart(2, '0')
+    row.start_hour = row.start_hour.toString()
+    row.end_min = row.end_min.toString().padStart(2, '0')
+    row.end_hour = row.end_hour.toString()
     row.dow = dows[row.dow]
-    if (row.end_min === 0) row.end_min = '00'
-    if (row.start_min === 0) row.start_min = '00'
-    row.gridSpan = {
-      begin: hourMap[row.start_hour] + minMap[row.start_min], 
-      span: hourMap[row.end_hour] + minMap[row.end_min]
-    }
-    r.push(row)
-  }
-  res.json(r)
+    row.gridStart = hourMap[row.start_hour] + minMap[row.start_min]
+    row.gridEnd = hourMap[row.end_hour] + minMap[row.end_min]
+    return row
+})
+
+  res.json(result)
 })
 
 server.listen(PORT, () => console.log(`Server listening on ${PORT}...`));
