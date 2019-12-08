1de6f27ef23849221dc285a7ff992988e4cf97dc
Zachary Olpin <info@zacharyolp.in>
2019-12-08 05:31:40 -0700

    turn start/end Date objects into ISOstring before inserting into DB

--- src/server/server.js
+++ src/server/server.js
@@ -32,13 +32,78 @@ server.post('/uploadFile', upload.single('file'), async (req, res) => {
   const result = await pool.query('SELECT employees.name FROM employees ORDER BY employees.name DESC')
   const employees = result.rows.map(obj => obj.name)
 
-  const buildingMapping = {
+  // TODO: Make a dictionary of equivalencies for e.g. building names, employee names/nicknames, etc.
+
+  const buildingNameMap = {
     'abravanel': 'abravanel hall',
     'capitol': 'capitol',
     'delta hall': 'delta hall',
     'rsbb': 'regent street',
     'rose': 'rose wagner'
   }
+
+  if (req.file.mimetype === 'text/csv') {
+    const csvString = req.file.buffer.toString()  // Single string representation of CSV
+    const parsed = parseCsv(csvString) // Makes 2D array representation of csv, with each inner array representing a row
+    const formattedDates = dateHeadersToISO(parsed)  // Turn column headers into iso timestamps
+    const filled = fillRowHeaders(formattedDates) // Fills blank first indices in each row with building name
+    const amPm = convertTo24Hour(filled) // Identify AM or PM for cells with times
+    const shifts = makeShiftObjs(amPm) // Make array of objects {employeeName, startTime, startDate, building}
+    const errors = []
+
+    // Date range from csv.
+    const earliestShift = dfns.min(shifts.map(shift => shift.startTime))
+    const latestShift = dfns.max(shifts.map(shift => shift.startTime))
+
+    // Select most recent date from shifts in DB
+    const lastDate = await pool.query('SELECT MAX(start) FROM shifts')
+    
+    // TODO: Test shifts, see if catching all, starttimes are correct etc etc
+    
+    // TODO: For all shifts, check name. If name in current list of employees, great. If not, 
+    // figure out who it belongs too. Rule out those scheduled concurrently elsewhere and those scheduled for same shift.
+    // After ruling out as many as possible, check Levenshtein ratio for remaining names.
+    // If ratio very high for only one name, add to that employee with a note (for displaying to the user that it is uncertain).
+    // If ratio does not obviously point to one name, add shift as unowned (display to all candidate owners as possible shift)
+    
+    // TODO: Check if csv's dates already in DB. If so, ask if user would like to replace existing shifts
+
+    shifts.map(async s => {
+      // Add employees to database
+      await pool.query(
+        'INSERT INTO employees (name) values ($1) ON CONFLICT DO NOTHING', // If name exists, don't add again
+        [s.employeeName]
+      ).catch(e => errors.push(e))
+
+      // add shifts to database
+      await pool.query(
+        'INSERT INTO shifts (building_id, start, "end", employee_id) values (\
+          (select building_id from buildings where buildings.name=$1),\
+          $2,\
+          $3,\
+          (select employee_id from employees where employees.name=$4 limit 1)\
+        )',
+        [buildingNameMap[s.building],
+        s.startTime.toISOString(),
+        s.endTime.toISOString(),
+        s.employeeName]).catch(e => errors.push(e))
+    })
+
+    res.json({errors: errors, shifts: shifts.map(shift => [shift.employeeName, shift.startTime, shift.building])})
+
+  } else {
+    res.status(400).send('File must be CSV')
+  }
+})
+
+// Get all employee names
+server.get('/employees', async (_req, res) => {
+  const result = await pool.query('SELECT employees.name FROM employees ORDER BY employees.name DESC')
+  res.json(result.rows)
+})
+
+// Get all shifts for an employee by name
+server.get('/employees/:name/shifts', async (req, res) => {
 
   // const getShiftsFromRange = async (startDate, endDate) => {
   //   const result = await pool.query('SELECT employees.name,\
@@ -54,53 +119,6 @@ server.post('/uploadFile', upload.single('file'), async (req, res) => {
   //   return result.rows
   // }
 
-  const determineEmployee = shift => {
-    // TODO
-  }
-
-  if (req.file.mimetype === 'text/csv') {
-    const csvString = req.file.buffer.toString()  // Single string representation of CSV
-    const parsed = parseCsv(csvString) // Makes 2D array representation of csv, with each inner array representing a row
-    const formattedDates = dateHeadersToISO(parsed)  // Turn column headers into iso timestamps
-    const filled = fillRowHeaders(formattedDates) // Fills blank first indices in each row with building name
-    const amPm = convertTo24Hour(filled) // Identify AM or PM for cells with times
-    const shifts = makeShiftObjs(amPm) // Make array of objects {employeeName, startTime, startDate, building}
-    const errors = []
-
-    // Date range from shift objects.
-    const earliestShift = dfns.min(shifts.map(shift => shift.startTime))
-    const latestShift = dfns.max(shifts.map(shift => shift.startTime))
-    
-    // TODO: Test shifts, see if catching all, starttimes are correct etc etc
-    // TODO: For all shifts, check name. If name in current list of employees, great. If not, 
-    // figure out who it belongs too. Rule out those scheduled concurrently elsewhere and those scheduled for same shift.
-    // After ruling out as many as possible, check Levenshtein ratio for remaining names.
-    // If ratio very high for only one name, add to that employee with a note (for displaying to the user that it is uncertain).
-    // If ratio does not obviously point to one name, add shift as unowned (display to all candidate owners as possible shift)
-    
-    // Inserts all employee names into DB (doesn't check beyond ON CONFLICT DO NOTHING)
-    shifts.map(async s => {
-      await pool.query('INSERT INTO employees (name) values ($1) ON CONFLICT DO NOTHING', [s.employeeName]).catch(e => errors.push(e))
-      await pool.query('INSERT INTO shifts (building_id, start, "end", employee_id) values ((select building_id from buildings where buildings.name=$1), $2, $3, (select employee_id from employees where employees.name=$4 limit 1))', [buildingMapping[s.building], s.startTime, s.endTime, s.employeeName]).catch(e => errors.push(e))
-    })
-
-    res.json({errors: errors, shifts: shifts.map(shift => [shift.employeeName, shift.startTime, shift.building])})
-
-
-
-  } else {
-    res.status(400).send('File must be CSV')
-  }
-})
-
-// Get all employee names
-server.get('/employees', async (_req, res) => {
-  const result = await pool.query('SELECT employees.name FROM employees ORDER BY employees.name DESC')
-  res.json(result.rows)
-})
-
-// Get all shifts for an employee by name
-server.get('/employees/:name/shifts', async (req, res) => {
   const result = await pool.query('\
     SELECT\
       shifts.start,\
