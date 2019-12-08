42712a7b972fde62c79a68b4734c04d84c4b8a82
Zachary Olpin <info@zacharyolp.in>
2019-12-08 05:30:05 -0700

    commands to reset DB shifts/employee tables/sequences

--- src/server/schema.sql
+++ src/server/schema.sql
@@ -16,3 +16,8 @@ INSERT INTO shifts (building_id, start, "end", employee_id) VALUES
   ((SELECT building_id from buildings where name='capitol'), '2019-10-19 17:45', '2019-10-19 20:30', (select employee_id from employees where name='zach')),
   ((SELECT building_id from buildings where name='delta hall'), '2019-10-20 09:45', '2019-10-20 14:00', (select employee_id from employees where name='zach'));
 
+-- Clear employee and shift tables/sequences
+alter sequence shifts_shift_id_seq restart with 1;
+alter sequence employees_employee_id_seq restart with 1;
+delete from shifts where true;
+delete from employees where true;
